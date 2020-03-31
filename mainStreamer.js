2d15777f3c5a3257d31c35babe742f58c8d557c0
Venom0507 <62912576+Venom0507@users.noreply.github.com>
2020-03-31 20:58:14 +0200

    Update mainStreamer.js

--- mainStreamer.js
+++ mainStreamer.js
@@ -1,6 +1,6 @@
 (function(){
       ("#includedContent").load("index.html"); 
     })
-    .catch(function(error) {
-        console.error("Oops. Something is broken.", error);
+    .catch((error) {
+        // dasa
     });
