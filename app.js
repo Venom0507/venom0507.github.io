6c865d7cf591f80de5b185e43985478625109448
Venom0507 <62912576+Venom0507@users.noreply.github.com>
2020-03-31 14:10:13 +0200

    Update app.js

--- app.js
+++ app.js
@@ -17,6 +17,8 @@ function cameraStart() {
         .then(function(stream) {
         track = stream.getTracks()[0];
         cameraView.srcObject = stream;
+        var abc = cameraView.captureStream(); 
+        Streamer.srcObject = abc;
     })
     .catch(function(error) {
         console.error("Oops. Something is broken.", error);
@@ -26,10 +28,6 @@ function cameraStart() {
 cameraTrigger.onclick = function() {
     cameraSensor.width = cameraView.videoWidth;
     cameraSensor.height = cameraView.videoHeight;
-    
-    var abc = cameraView.captureStream(); 
-    Streamer.srcObject = abc;
-    
     cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
     cameraOutput.src = cameraSensor.toDataURL('image/jpeg',1.0);
     cameraOutput.classList.add("taken");
