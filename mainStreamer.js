d44c29a593d1d413acd107ea63ce23a40230079f
Venom0507 <62912576+Venom0507@users.noreply.github.com>
2020-03-31 21:14:58 +0200

    Update mainStreamer.js

--- mainStreamer.js
+++ mainStreamer.js
@@ -2,7 +2,7 @@ const streamerView = document.querySelector("#streamer--view");
 
 (function streamStart(){
       ("#camera--view").load("index.html");
-      StreamerView.srcObject = camera--view;
+      StreamerView.srcObject = #camera--view;
     });
 
 // Start the video stream when the window loads
