8e3ed0de64d70ff3c2f14e088d691fb1ee3b2e1c
Venom0507 <62912576+Venom0507@users.noreply.github.com>
2020-04-05 21:06:10 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,27 +4,30 @@ var constraints = { video: { facingMode: "user" }, audio: false };
 
 // Define constants
 const cameraView = document.querySelector("#camera--view"),
+      streamView = document.querySelector("#stream--view"),
       cameraOutput = document.querySelector("#camera--output"),
       cameraSensor = document.querySelector("#camera--sensor"),
       cameraTrigger = document.querySelector("#camera--trigger");
 
 // Access the device camera and stream to cameraView
 
-function cameraStart() {
-    navigator.mediaDevices
-        .getUserMedia(constraints)
-        .then(function(stream) {
-            track = stream.getTracks()[0];
-            cameraView.srcObject = stream;
-            const myMediaSource = new MediaSource();
-            const url = URL.createObjectURL(myMediaSource);
-            // attaching the MediaSource to the video tag
-            cameraView.src = url;
-    })
-    .catch(function(error) {
-        console.error("Oops. Something is broken.", error);
-    });
+if(document.URL.indexOf("index.html") >= 0){ 
+      function cameraStart() {
+            navigator.mediaDevices
+            .getUserMedia(constraints)
+            .then(function(stream) {
+                  track = stream.getTracks()[0];
+                  cameraView.srcObject = stream;
+            })
+            .catch(function(error) {
+                  console.error("Oops. Something is broken.", error);
+            });
+      }
+      // Start the video stream when the window loads
+      window.addEventListener('load', cameraStart, false);
 }
 
-// Start the video stream when the window loads
-window.addEventListener('load', cameraStart, false);
+if(document.URL.indexOf("Streamer.html") >= 0){ 
+      streamView.srcObject = cameraView;
+}
+
